c7683afab9bd2533b5b7b38eefd52bf27c46d31c
Yohannes91 <63756368+Yohannes91@users.noreply.github.com>
2022-09-18 14:05:00 -0400

    john third branch fix

--- etlpython.py
+++ etlpython.py
@@ -1,5 +1,9 @@
-import numpy as np
-import glob as glob 
-from matplotlib.pyplot import matplotlib as plt 
-import pandas as pd 
+# import numpy as np
+# import glob as glob 
+# from matplotlib.pyplot import matplotlib as plt 
+# import pandas as pd 
+
+import time as time 
+time.sleep(4) 
+print('Hello Test')
 
